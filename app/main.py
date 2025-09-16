5bc886c31df635ccf2f3ef2cdb51b9e07a667849
Matt Meyer <matthewwilliammeyer@gmail.com>
2025-09-16 12:00:59 -0400

    inmem todo

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from typing import List, Optional
 
 from .routers import items, users
 
@@ -8,14 +9,56 @@ import strawberry
 from strawberry.fastapi import GraphQLRouter
 
 
+# In-memory storage for todos (replace with database in production)
+todos_storage = [
+    {"id": 1, "task": "Buy milk"},
+    {"id": 2, "task": "Read a book"}
+]
+next_todo_id = 3
+
+
+@strawberry.type
+class Todo:
+    id: int
+    task: str
+
+
 @strawberry.type
 class Query:
     @strawberry.field
     def hello(self) -> str:
         return "Hello World"
 
+    @strawberry.field
+    def todos(self) -> List[Todo]:
+        return [Todo(id=todo["id"], task=todo["task"]) for todo in todos_storage]
 
-schema = strawberry.Schema(Query)
+    @strawberry.field
+    def todo(self, id: int) -> Optional[Todo]:
+        todo_data = next((todo for todo in todos_storage if todo["id"] == id), None)
+        return Todo(id=todo_data["id"], task=todo_data["task"]) if todo_data else None
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.field
+    def add_todo(self, task: str) -> Todo:
+        global next_todo_id
+        new_todo = {"id": next_todo_id, "task": task}
+        todos_storage.append(new_todo)
+        todo = Todo(id=next_todo_id, task=task)
+        next_todo_id += 1
+        return todo
+
+    @strawberry.field
+    def delete_todo(self, id: int) -> bool:
+        global todos_storage
+        original_length = len(todos_storage)
+        todos_storage = [todo for todo in todos_storage if todo["id"] != id]
+        return len(todos_storage) < original_length
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 graphql_app = GraphQLRouter(schema)
 
